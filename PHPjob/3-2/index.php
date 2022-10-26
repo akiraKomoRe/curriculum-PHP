1dc769983f6f391f8a2c0cf50bb0402d06370743
akriaKomori <112708311+akriaKomori@users.noreply.github.com>
2022-10-26 18:34:11 +0900

    ３章-練習問題1提出

---
+++ PHPjob/3-2/index.php
@@ -0,0 +1,17 @@
+<?php
+$fruitsPrice = ["apple" => 150, "orange" => 50, "peach" => 300];
+$fruitsPiece = [2, 3, 10];
+function furuitsValue($unit, $quantity) {
+  $price = $unit * $quantity;
+  return $price;
+}
+
+$i = 0;
+
+foreach($fruitsPrice as $key => $value) {
+  $total = furuitsValue($value, $fruitsPiece[$i]);
+  echo $key."は".$total."円です";
+  $i++;
+  echo "<br />";
+}
+?>
