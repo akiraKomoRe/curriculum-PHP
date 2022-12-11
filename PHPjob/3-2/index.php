1c445ca18e53809c9d1ed4f8b9ae97707d524946
akriaKomori <112708311+akriaKomori@users.noreply.github.com>
2022-12-12 05:05:16 +0900

    3-2の修正をしました。
    確認をお願いいたします。

--- PHPjob/3-2/index.php
+++ PHPjob/3-2/index.php
@@ -1,17 +1,19 @@
 <?php
-$fruitsPrice = ["apple" => 150, "orange" => 50, "peach" => 300];
-$fruitsPiece = [2, 3, 10];
+
+$fruits = ["apple" => 200, "orange" => 50, "peach" => 600];
+$piece = [20, 100, 1000];
+
 function furuitsValue($unit, $quantity) {
   $price = $unit * $quantity;
   return $price;
 }
 
-$i = 0;
-
-foreach($fruitsPrice as $key => $value) {
-  $total = furuitsValue($value, $fruitsPiece[$i]);
+foreach($fruits as $key => $value) {
+  $i = 0;
+  $total = furuitsValue($value, $piece[$i]);
   echo $key."は".$total."円です";
   $i++;
   echo "<br />";
 }
+
 ?>
