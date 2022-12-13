291d14bbb651288efed74aa3edf6a2de6301609f
akriaKomori <112708311+akriaKomori@users.noreply.github.com>
2022-12-13 19:51:07 +0900

    4-1,4-2課題提出

--- PHPjob/4-2/index.php
+++ PHPjob/4-2/index.php
@@ -13,8 +13,22 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
+  <header>
+    <div>
+      <h1><img src="logo.png" alt=""></h1>
+    </div>
+    <div>
+      <div>
+        <p>ようこそ隼田翔伍さん</p>
+      </div>
+      <div>
+        <p>最終ログイン日：2019-07-02 21:56:05</p>
+      </div>
+    </div>
+  </header><!-- /.header -->
   <table>
     <tr>
       <th>記事ID</th>
@@ -39,5 +53,10 @@
     </tr>
     <?php } ?>
   </table>
+  <footer>
+    <div class="footer__logo">
+      Y&I group.inc
+    </div><!-- /.footer__logo -->
+  </footer>
 </body>
 </html>
